58573e24089532eeca0874727387546d084ac95c
Arseniy Latyshev <landih73@gmail.com>
2023-02-01 15:42:25 +0300

    SEDMVDDEV-3874 Актуализация для Prod&Dev

--- src/main.js
+++ src/main.js
@@ -1,27 +1,21 @@
 import { createApp, h } from "vue";
 import App from "./App.vue";
 
-//createApp(App).mount('#app')
-
-// export const HeaderFooter = Vue.extend({
-//     render: h => h(App),
-// })
-// let HeaderFooter=require("vue");
-if (import.meta.env.DEV) {
-  const HeaderFooter = createApp({
-    data: () => ({
+const HeaderFooter = createApp({
+  data: () => ({
     //   onSave: () => console.log("onSave callback"),
-      fields: {
-        logo: { url: "http://localhost:8080", title: "Главная страница" },
-        service_name: "Сервис электронного документооборота",
-        right_part: [],
-        has_footer: true,
-      },
-    }),
-    render: () => h(App),
-  });
+    fields: {
+      logo: { url: "http://localhost:8080", title: "Главная страница" },
+      service_name: "Сервис электронного документооборота",
+      right_part: [],
+      has_footer: true,
+    },
+  }),
+  render: () => h(App),
+});
 
-  // window.HeaderFooter = HeaderFooter;
-
-  HeaderFooter.mount("#app");
+if (!import.meta.env.DEV) {
+  window.HeaderFooter = HeaderFooter;
 }
+
+HeaderFooter.mount("#app");
