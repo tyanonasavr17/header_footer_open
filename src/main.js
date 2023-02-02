e360b1d10155c153713e60a266e06706f1d0f221
Мамутова Влада Александровна <vmamutova@phoenixit.ru>
2023-02-03 00:04:15 +0300

    SEDMVDDEV-3874 Добавить функцию создания элементов и пример в test.html

--- src/main.js
+++ src/main.js
@@ -1,55 +1,62 @@
 import { createApp, h } from "vue";
 import App from "./App.vue";
 
-const HeaderFooter = createApp({
-  data: () => ({
-    //   onSave: () => console.log("onSave callback"),
-    fields: {
-      logo: { url: "http://localhost:8080", title: "Главная страница" },
-      service_name: "Сервис электронного документооборота",
-      right_part: [
-        {
-          type: "button",
-          icon: "btn-folder",
-          url: "http://localhost:8080",
-          title: "Архив долгосрочного хранения",
-        },
-        {
-          type: "button",
-          icon: "btn-home",
-          url: "http://localhost:8080",
-          title: "Рабочее место исполнителя",
-        },
-        {
-          type: "message",
-          icon: "btn-message",
-          url: "http://localhost:8080",
-          title: "Беседы",
-          message_new: true,
-        },
-        {
-          type: "profile",
-          text: "Иванов И. И.",
-          url: "http://localhost:8080",
-          title: "Профиль",
-        },
-        {
-          type: "logout",
-          icon: "btn-logout",
-          url: "http://localhost:8080",
-          title: "Выход",
-          method: "delete",
-          name: "Выйти из системы",
-        },
-      ],
-      has_footer: true,
-    },
-  }),
-  render: () => h(App),
-});
+window.createHeader = ({ data = {}, element = null } = {}) => {
+  if (data == null || element == null) {
+    console.error("Failed to create widget: data or element is not defined!");
+    return;
+  }
 
-if (!import.meta.env.DEV) {
-  window.HeaderFooter = HeaderFooter;
+  createApp({
+    data: () => (data),
+    render: () => h(App)}
+  ).mount(element);
 }
 
-HeaderFooter.mount("#app");
+if (import.meta.env.DEV) {
+  createApp({
+    data: () => ({
+      fields: {
+        logo: { url: "http://localhost:8080", title: "Главная страница" },
+        service_name: "Сервис электронного документооборота",
+        right_part: [
+          {
+            type: "button",
+            icon: "btn-folder",
+            url: "http://localhost:8080",
+            title: "Архив долгосрочного хранения",
+          },
+          {
+            type: "button",
+            icon: "btn-home",
+            url: "http://localhost:8080",
+            title: "Рабочее место исполнителя",
+          },
+          {
+            type: "messages",
+            icon: "btn-message",
+            url: "http://localhost:8080",
+            title: "Беседы",
+            message_new: true,
+          },
+          {
+            type: "profile",
+            text: "Иванов И. И.",
+            url: "http://localhost:8080",
+            title: "Профиль",
+          },
+          {
+            type: "logout",
+            icon: "btn-logout",
+            url: "http://localhost:8080",
+            title: "Выход",
+            method: "delete",
+            name: "Выйти из системы",
+          },
+        ],
+        has_footer: true,
+      },
+    }),
+    render: () => h(App),
+  }).mount("#app");
+}
