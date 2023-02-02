6680ad54b41cfd1c1e3dab918010825564c505ef
Arseniy Latyshev <landih73@gmail.com>
2023-02-02 19:43:18 +0300

    Ушли от разных компонентов к унифицированому TagByButton

--- src/components/RightPart.vue
+++ src/components/RightPart.vue
@@ -1,11 +1,3 @@
-<script setup>
-import defaultButton from "/src/components/RightButtons/defaultButton.vue";
-import Message from "/src/components/RightButtons/Message.vue";
-import vispCustom from "/src/components/RightButtons/vispCustom.vue";
-import Profile from "/src/components/RightButtons/Profile.vue";
-import Logout from "/src/components/RightButtons/Logout.vue"
-</script>
-
 <template>
   <div class="right-part">
     <div
@@ -13,68 +5,65 @@ import Logout from "/src/components/RightButtons/Logout.vue"
       :key="button"
       class="section"
     >
-      <span v-if="button.type == 'profile'">
-        <Profile
-          :url="button.url"
-          :name="button.text"
-        />
-      </span>
-      <span v-else-if="button.type == 'logout'">
-        <Logout
-          :url="button.url"
-          :name="button.text"
-          :title="button.title"
-          :method="button.method"
-        />
-      </span>
-      <span v-else-if="button.type == 'message'">
-        <Message
-          :url="button.url"
-          :message-new="button.message_new"
-          :title="button.title"
-        />
-      </span>
-      <span v-else-if="button.type == 'extra_div'">
-        <vispCustom
-          :url="button.url"
-          :icon="button.icon"
-          :code="button.code"
-          :title="button.title"
-        />
-      </span>
-      <span v-else>
-        <defaultButton
-          :url="button.url"
-          :icon="button.icon"
-          :title="button.title"
-        />
-      </span>
+      <TagByButton :button="button" />
     </div>
   </div>
 </template>
 
 <script>
+import defaultButton from "/src/components/RightButtons/defaultButton.vue";
+import Message from "/src/components/RightButtons/Message.vue";
+import vispCustom from "/src/components/RightButtons/vispCustom.vue";
+import Profile from "/src/components/RightButtons/Profile.vue";
+import Logout from "/src/components/RightButtons/Logout.vue";
+
+import { h } from "vue";
 
 function ComponentByType(type) {
   const Types = {
     message: Message,
-    'extra_div': vispCustom,
+    extra_div: vispCustom,
     profile: Profile,
     logout: Logout,
+  };
+  var Component = Types[type];
+  if (Component == null) {
+    return defaultButton;
+  } else {
+    return Component;
   }
-  return Types[type]
 }
-
+const TagByButton = {
+  props: ["button"],
+  render() {
+    return h(
+      "span",
+      h(ComponentByType(this.button.type), {
+        url: this.button.url,
+        name: this.button.text,
+        title: this.button.title,
+        "message-new": this.button.message_new,
+        method: this.button.method,
+        code: this.button.code,
+        icon: this.button.icon,
+      })
+    );
+  },
+};
 export default {
+  name: "RightPart",
+  components: {
+    TagByButton,
+  },
   props: {
     headerButtons: {
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  @import "src/styles/right_part.scss";
+@import "src/styles/right_part.scss";
 </style>
