e1813807d61c258ace783150a2edef6043ceb985
Denis Savosin <dsavosin@at-consulting.ru>
2023-02-03 17:38:41 +0300

    Добавил внедрение css стилей в js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,8 @@ export default defineConfig({
         }
       }
     }),
-    svgLoader()
+    svgLoader(),
+    cssInjectedByJsPlugin(),
   ],
   build: {
     manifest: true,
