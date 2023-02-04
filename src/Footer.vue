39f0f0b771d4279744de87573644ae25f4a4d687
Мамутова Влада Александровна <vmamutova@phoenixit.ru>
2023-02-04 21:25:58 +0300

    SEDMVDDEV-3875 Упрощение структуры футера и изменение расстояния от вертикальной черты

--- src/Footer.vue
+++ src/Footer.vue
@@ -4,23 +4,17 @@
     class="footer"
   >
     <div class="container">
-      <div class="row-fluid">
-        <div class="span6">
-          <div>&copy; Сервис электронного документооборота МВД России, дата</div>
+      <div>&copy; Сервис электронного документооборота МВД России, дата</div>
+      <div class="footer-right-div">
+        <div class="text-right">
+          Круглосуточная служба поддержки ЕЦЭ ИСОД МВД России<br>
+          8 (800) 2000-462 (звонок бесплатный), 8 (495) 667-67-17
         </div>
-        <div class="span6 text-right">
-          <div class="footer-right-div">
-            <div>
-              Круглосуточная служба поддержки ЕЦЭ ИСОД МВД России<br>
-              8 (800) 2000-462 (звонок бесплатный), 8 (495) 667-67-17
-            </div>
-            <div class="divider" />
-            <div>
-              <a href="https://it.mvd.ru/profile/requests/new">helpdesk@mvd.ru</a>
-              <br>
-              <a href="mailto:helpdesk@mvd.ru">Отправить обращение</a>
-            </div>
-          </div>
+        <div class="divider" />
+        <div class="text-right">
+          <a href="https://it.mvd.ru/profile/requests/new">helpdesk@mvd.ru</a>
+          <br>
+          <a href="mailto:helpdesk@mvd.ru">Отправить обращение</a>
         </div>
       </div>
     </div>
